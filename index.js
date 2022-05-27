b8695e23644da750c1a41eb31dd4cf4614796f84
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2022-05-27 15:54:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const exemple = 'eu vou para a escola'
-let vowel = ['a', 'e', 'i', 'o', 'u']
+"use strict";
 
-let howManyVowels = 0
+const exemple = 'eu vou para a escolaa';
 
-function vowelCounter (){
-  for (let i = 0; i < exemple.length; i++){
-      for (let v = 0; v < vowel.length; v++){
-        if (exemple[i]===vowel[v]) {
-            howManyVowels ++
-        }
-      }
-  }
-  return howManyVowels
+// Criando função para todas as variaveis que for do tipo String.
+String.prototype.countVowel = function () {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  // Para usar esta função no prototype, coloca o nome da variavel.função. o this é igual ao valor da mesma. 
+  // Agora eu pego o valor ou seja "eu vou para a escolaa" e espalho ele dentro da outra variavel
+  const vowelStr = [...this];
+  
+  //Number e o + são apenas para converter para String
+  let count = Number(+0);
+  
+  //Percorrenndo o array usando o forEach
+  vowelStr.forEach((letter) => {
+    //Ternario, se a letter da posição exister dentro do array então retorna o count++ se não false. o + a esquerda apenas para converter para num
+    vowels.includes(letter) ? +count++ : false; 
+  });
+
+  //Retorna valor do count
+  return +count;
 }
 
-console.log(`A frase "${exemple}" tem ${vowelCounter()} vogais`);
+// Chamando a variavel que esta no prototype
+console.log(`A frase "${exemple}" tem ${exemple.countVowel()} vogais`);
+
+// No prototype tem as função dos tipos. Exemplo no tipo String temos diversas função la, tipo a .length, e etc. a função length.
